e8de713427f2b6dab8b4569a1bbaf69154311f72
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-01-01 05:27:48 -0600

    Add subcommands for TravisCI, Coveralls, AppVeyor

--- lib/Oberth/CLI.pm
+++ lib/Oberth/CLI.pm
@@ -8,6 +8,10 @@ use CLI::Osprey;
 subcommand 'github-issue-count' => 'Oberth::CLI::Command::NumberOfGitHubIssues';
 subcommand account => 'Oberth::CLI::Command::Account';
 
+subcommand 'travis-ci' => 'Oberth::CLI::Command::TravisCI';
+subcommand 'coveralls' => 'Oberth::CLI::Command::Coveralls';
+subcommand 'appveyor' => 'Oberth::CLI::Command::AppVeyor';
+
 method run(@) {
 	...
 }
